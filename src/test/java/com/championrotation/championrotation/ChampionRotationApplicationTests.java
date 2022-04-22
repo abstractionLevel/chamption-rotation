d818ff0f69d8cbdc522f1a3bd9bd5318bf126306
sky <marcodescrescendo@gmail.com>
2022-04-22 21:23:03 +0200

    first comment

---
+++ src/test/java/com/championrotation/championrotation/ChampionRotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.championrotation.championrotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionRotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
